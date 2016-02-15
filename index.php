920e5ec0a5e12b510485ffce63b2d2ab9ac7da3b
raincoats <raincoats@users.noreply.github.com>
2016-02-15 18:55:53 +1100

    init

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php
+
+$host = $_SERVER['HTTP_HOST'];
+
+$ducky = 'lVYJOJRt939mxjAIM5YsYxlky9IMyZ41UsRMsoeZsWWPsUVlK0uKpCSSITJFErJv2UKR'.
+         'iLGUUMgSokSWb0bv+/a+/+v/fdf3nZl7zu/6nTP3c55zn/vc9w515wMAP2ZobAiAwAAA'.
+         'on2AnQUgxtzdl+Qb4O7rh1JSQAOqesamDAwAXdwEwdIAwVDP1ACNQaPReDwerUTTaCIB'.
+         'rUzXeEW0Cl0rq6FV6VrVFa22yyuj8XRNcEYTd3k02pmuDyqhXXb9D6Fdd+2qGEW6BnYm'.
+         'wILG+vqO5jgzQ2OTIwCIFhl4jxfROwAMBwBvH5I/zkgPZW1ji2IapUXND0AAGUANTwzw'.
+         '0zU3NwH+rXwfoL8jAPTJ0+f6937/r7A4uwQQaXqTNhyIfv4kWsrsaZg1mORHx+40zE3w'.
+         '3MXhdOxPC5CGk+nY7Re+v+vzC1fu+ljg9Gn4JQ3ruP0NE/6G/3guXSBExf8x6v9CSC4h'.
+         'JLo21AMAek5/sb9z9ZuD7KUNJdpa3P/N+eYAgOo3Gp/8myOkA0DFJQDgG/7N7csGAI4Y'.
+         'AHjWQwz0D/rj0QK00QXqgBChKTBP1u9wbR4s8qDoskyyIp9GiiEUG3x6wdPvHDS+5E54'.
+         '4ZmG833Nn9E7OzsjgD7AzMgEY2JkhsFgLCzMrOy8HOxsbOyC3DwIXjFhcXFRYRRKYr+y'.
+         'vISUojQKpaB9QFFFTVNTc5+8nqGuuqGyhqYafRIQCwsL+x52JAcHUk0SJan2P8vOcwAB'.
+         'A0aBUQggBoAQIDACtNMCIGmJg4BAINDvFIMhDFAQIxPNKAD6ZQT+r/E/2oaBPRBaOSDA'.
+         'CNrvz4h/LJ/Mo6FeA7Xg03LbJ+W/fy0ccjRmXn3IobNrW2m1ke9qfNYRsO1YylFVPF4/'.
+         'nU4SdwkprHDsejv66nuu/a4XgxTL0Po7vs23m2bxWypTYa/XHd888Z628/HqV/ZZH6k6'.
+         'R36/cfHV9gMXEZwCvSIBmNkLxTbses3yxFAG+fHm/PW7tQWD+Xzk6pqCR3NVO4CjSwCv'.
+         '+e7UWMPRcDv3pPKA1a+YYD9SoMv30LJmA79vUlb3HLvn3Dv3vqG/ZI3S2y9HBx1ZrfTN'.
+         'OE7MFvU85p9O0jZT0FAd0kAMfdJ2bFpcDdhQHqBPyFLUYVA6dbNWKGRz0P71yK3A9WJ1'.
+         'zIpcmdW5rGKlEeLIPdag2Yqrb+kpaq5RsPn03f/DTI6AZphZ/cZaezHseMejry9+LJ2b'.
+         'PR0bfBHlCAcAxvPcfV/ODPp2j8GGmMNgNhxuc61m4d88EE+skLIKs/7qW17j9U9o06HN'.
+         'DB143iTwSyZKOSffDOzqF+XwXVO5I3peeX3OyPfK8Kw58hHNDXQ6Tm1pZvLjWMLHqjUs'.
+         'h7T8Vcy9NMOCnq7kL54x3+56je/uB5Dl+YcSLdNbjqldk/fwi8W3Zr5bttWw5gj/Y21p'.
+         'NSEFB8BgEIQB/Fdh0Ho1BMEA5xRFY3SgXCgxRSduJVqBwGhWEBS8AzBYw7zveZpVs3Jv'.
+         'zL/elsFbsxvlt1mP88gEZXrirEQiSmQeP8J9XB0r2gEOULy1BFZ78yw8fLKjk4PW1V/d'.
+         '8Y1+67QhP+Fx6sGL5X4sxd5UW8V5y6LZo9uD0F/tfXRdQw3rMeZ4vtYrZGLsatG1DFPL'.
+         'TDcEtaxNosLQq+kwf/XaSItisxKnhv++idPB0q+e3ENgLVZNnuhcp8IGHjIFPTsfMHs5'.
+         'n0uXvDcCEI6Ow1Mp8MGBszatUnPSVRRnH+MZvtfz74ikmLYAtZJUd4Wyk+SH9T+FRQsl'.
+         '/THyiWqGfNS451oBnyXPFAjG6o7VrF/2vzmrmrQhq7QagrN+Et92puHbqkizXmniJI+B'.
+         'Q4Z2ookZ1Pcon/DWQgSrmRJyys1L/8cp33OLc1xcQp0ZKWsKm5iR7G9f7CaJpSHzb3t/'.
+         '3tpTQJ0P50C6fTWbqzbaS20eDxdaUVJiTo/fCrb2R/K154RoTLm6unmrfnILTUOvDk1H'.
+         'mLykS3e/VVPYR1ctcwXRJLUTJ5WUMDyFB81lVzujBXKFH4nLMg9caFPImro5E3PKQrie'.
+         '6YcvbWX3IX6tLOT3hqc1EAgKrQvnEsVw6mAV8bvLSmsq2qANYxtBzxsVnNYNhwZjwtOZ'.
+         'HU2udHd+HgmHtxv4back8m44Gz8qSjUsiAmVunGpb9+PoWj4A/6HxBj49FV+N/5ztm+e'.
+         'hWOl/eN6Vks60rXmj9hJk8kGzmTnkoHai70usykPHh5uEz3Xl/a+srMlYUme3/Fu3Ef7'.
+         'tDddEurIIZz7l9JKNW0KLWRpBABiAEEgEOifIYNoHQrBwCmK4UKh4dy6UDFFHeyvagTR'.
+         'w96SkfAijN7fiE6afMr/dAewsFkrfa1Suaw2LeYhO3Lk1HFZPsOgcVzLhYL2y46NBe9t'.
+         'fzKne4l1dnbMVjIuZfB+0+d3g6IXHZiuMMgfJMu/jiF8N/X2L8I0HBacIWlYf8j+yo2b'.
+         '1cfqqfAgj4cupn9KVGAtWEOW7xm+HB9ychKTe5LlDPbl4qWEQLBZtFi168CCbd98/p0h'.
+         'pQXsR77uTrm2yt7Frh1ALNO8wc7q87Wts9HvVPuf8/bkZnjMpUoXSZc7iTytWUF9rm5n'.
+         'xbGzGPQPink7PbttFkYVlqYsHQ/2ZX6evDfanBDLV6VMzom99GO5IbnuxmF4C3PlvAg9'.
+         'XQfgAAgCZmCAwCC/04UQ1YHAMViUGBrPyaXILa6kq4dzOmtOiPxjDzNqgzdZHkbvv5Jw'.
+         'NggnL2rsIGeE2LLNnmiiZvESNBt1f/jcyjCRHJBgHO9dXJ9/spn2Y/lEvjlbqNxPc7W+'.
+         'z4p16WMl7qOG56gu+EkV3WIXUcaDxjJFVJfHDp6O5E+sftEjrrrc6sVx+LBng4FqjT2I'.
+         'hoUKFwmjrkhq7Oybweb9eUGnuo5MEYQdy3XeDJBPPHKxXz84xOTrbU9JvTHOYlK5Ntf5'.
+         '1qOqknLkIbKgLIrHxT8LEbq/zEIrhKXOOnhZ/7GRRWDrh1reqE71U/3Uvlw2FtPTZ/Tq'.
+         'ZE0K3tQhftp/yWrSN+Bob6DXEpyWDBCYEQz6s5hAAEIUjtHRxeLPXkdHopzIJb3LTX92'.
+         'Nm3RLWjlQw5FrTjplTsrzX1xryqWG9Wix8VvJTYMu7CsoEOJk43E6aoBCyJTuk93iXGT'.
+         'qriiV3zjAGsSOGE+IN7rmn93cG9lZ/fcPvFDx6MwzWGtC1wPbXeAbtZgIysB0/qVvXak'.
+         'r4HRYFsifPoQ7MXepxvXeOK+p+lM5a5HPlPn8mJKM4oYG2TEku06ebR4pNWQddReNy4k'.
+         'Sf+sm3K79egwMi/bD1lNSaubcil1HyqDREh2wJp1L9lpsoM6DpFyEU5lQisHkk+Cj+wA'.
+         '/V3Oa4damfFC17bxKHJJ48c600TDjMWXeyeHwYT6G/WpXhd6H4iPlhRWPONPSEmzhz3N'.
+         'LnS31jVRaYzyurBkH4O1bGS8ULORlKr9oDjTaUq/hePeaJfDa4PA23esZVAat09tuRwg'.
+         'NQLUDwYJVV1cqBQL8nfJ4xuP9O6GPIaqiF8Xxpx5dYxFrLN5KLt6NEdFtnakfdo0ajyL'.
+         'YPMUfQeWdo0Uj8uTfOlZ07wiqKkny9txs/+m7JIxrpx8+qW2dOflm8hj+Xnhh/H30kLf'.
+         'c2b3S3zGNfB8vZI1dE2g4uBdWN1+DGHt3iHxNOkLBIe8g9YOPRn83d4xadz5ggUlxJwu'.
+         'FU2Itmro88KZovtsR5uzZ2zL+LMW/AAZMZ87UcV6BWtGS2ERTjsAvi96rGOrOjC25T3/'.
+         'N15yZOXh/Dd18KHkQvbIliAOR3LDP65E8JUezd/fxgIt//BdfFiihSdsl1zm7iBZW++S'.
+         'XOWdFYmLu56vHlteEqCD1lChbKXgXU9RZuWghmY62TKhw50Ij6CTwtuZJ1ockmhkl1RG'.
+         'bb5hyiaNXFINuZzMHrv7r6+gt7F/j6FHk1biKATtOkfrmJDfNzoQQhQDR6GxOrrkkr+6'.
+         'O/yHiXtcgdeGKJmS1JKBHFYOzkq+yLHvWO6BxrRQQp630Gzxc+f4YgeGVe/PxKcN/dcS'.
+         'bozXY9DJpg/MKEJyNf0xL2SYMcGssHBBxna/WvZjR+ScYR6cnHFTyvsX3aPPhqXen3MJ'.
+         'PmhpK0COWT1zTFIkXONF56HTjAf9sLznvNVjlRhL2BLREW1zS8svv4tWkIvj6fcOBIh+'.
+         'PIGhDL97Fy12LFoXRdueJTrk3vHIpqW/Oj18Q7bO4MRVLerebQapI6HJwO2ZIl5Q/t4k'.
+         'MQGtcxfDIt/15prHeLUfwWVqw29NeHguyt7kCGKxr6buwbVtCPtwiWmIKaZfvx26AFG0'.
+         'y1mtzWqoiuK3uxyEuNqUEbIG51INnhNSVNgBbjcyt06vcS4z4ipnSwNfk34mxBJY5GT5'.
+         'anOLSlZyoUIHKhLHlsJblTkTZgVb8/QYxZDL0plGqX3xTkfzsHodmdkJ9RxXZ+gHEeCq'.
+         'nqdg7XSjNK49uC113qXesXKzJftIxxWhK+/jjLC9risB0sMcvsOuHh8sOXJpCZHZ7Ve0'.
+         's++vA3s3IbpYOP6sTmQJ+jq5yal3CTX+V8OCbzMciYHAvkzYNp4nY9zO3oFeo5qamaZT'.
+         'Xh6XKKRgYdeJTRohsSntbyy3Op4EQYJmBB+3UDTPAEF6g1UnpZi01numIGQSIlFdeC5t'.
+         '5EOuw1u2tohtA//D8bBq/ni7QMJxm6KoCCzb8fuEXrj6tdx6ndhR74E2qw9EZPukRKbA'.
+         'xJe6fPsWTbWbpAP9cTORgkHI1AZh30dfz3MWCxnKG7Zm34HuZXblj6OGPSRGSYQ1rcIS'.
+         'Em5MN9XWyadPJUTJN5o1RR0Fx+NkjYatpBupN40nZiE2Nro3qFutUnjby7kzh5l6jGwK'.
+         'TeXYLAm1t2Z9Fy8WVBdYmt3d4taz1bdU+EDS9sofmY43f/Oup+VUjb78AyVVb5F7Oge+'.
+         'HHsmQHRPZDPWvZkZYPzzqrl6ofO4RsCUCrgqRgy5pyFMfqlBOMJaY8KS6WSuuimPoP3F'.
+         'HNdI6rvb4ati65sUrsKfkviYPtCefH/qwiMGig4L8FqLjXtmbkpTT72r9DwBbu1JSfIN'.
+         'm4mxpHCphTXyglkir3nfGttYuwW1fHX8wOsrRZPlHqSDP92DxbB2bq78fupNNyQm3E5P'.
+         'pmhwDJueSiZlNYuExQ64ltoKlmjotiE8Gbn5mVzfu3JOu5666je/Wihu27jC5wi6X6Bq'.
+         '2Bbp81onO3thBjpYe3QpbKYcmqcmai9OOcOaEHq0mJTKi1WkeAmXs+wA2sX65sERDg8w'.
+         'bCXMzuWh7Tq5ZWUFbyUKGfNO8NgsQNnGTRzLNkUa7CetbZdlHS58fWaeFSd17EnkoAyu'.
+         'AlHKyt2kot/BKcfOaze+MWCzbSdzxUN3m0p+HBMRKSTR+9mCUpBUK2LVURhlKZ6KMCnv'.
+         'Tpi9LnbssxX/9oULBUsoe3npNPbH4rhxPmIiXswgsfzr4JiIFWjwwy2E7lmZ9DR3Hyja'.
+         'w98AcyDbQjSSWPGK7XAOs4nlaOcCT8p4wdNltsYrAiRI9ikFgpjPYfw3s4gXL4ejPAyv'.
+         '2wSjfUQ2pXPkjPJCbLbKNrf85Rrf46AYF8ucTrBFO0U4GxpaxhBWJZQpHJWZIjA4XqWy'.
+         '/CmVcmYmSvH27dPdm8Knsxlrhsa3NjQ/cUjy374yAtO9KL7Mdb4Dbtu0cbyU8ElE+93t'.
+         'F58+aL27gEpZSDoRrWQCerDkuDP0Lw==';
+
+$favicon = 'tZRfSFNRHMdvIfgWgtCDytwkSEMTRWnSgy9C0EMRe+vJMNCHepCee+gPVDCEXkaQRH'.
+           '8fCiIS6amyWjWL2simzm1OrelS9/9ubrv3nvvpUASBm6OHvodzHn6Xzzm/7/dwj6Ls'.
+           'kqOuTpGrVRmpUZS9iqK0yilLsvK7/ks1yjbxH5QXJu6chmshxuV3fm56A8xuJDBMc2'.
+           'dQfp8rGZxZ07AFS9S619j9dIbaRx7an3i4F1xFF6IivqgZOFSDfTHBgaBOU0Cjxpuj'.
+           'fnqdxldL9L4OEc4VyrJCnu3M6HQXBIcTAnvEwBIxsYQF3QsafbN5urxxfNliWX7LEI'.
+           'wkDNqyJp0xE9uiwLJk0rpoYp3XaJjJ0+fPsFzQyvKqYTK4pGENy3P9JRr8Gk1BQfuc'.
+           'huVTmsYPCUYjKoUK/ouy/+FoiebpJF1TUboCWWzfZY7uKANvI9yNpokV9YrZaZK/8q'.
+           'PIcX+KayspjoTy7Hkfp+76Gxx3psgUyvv+WwV57xGZw0lVUP9Fp9m5SefQR+xDE1wa'.
+           '/0xKLe28gezhmfRnT5ocndTpd6bpHX5B24kxBk65CK5sVOVdwqAnUeTYLQ/7Hy5w6E'.
+           'aOjsFJWvrPM/F8pooDk/uSdyx/4+zoKD1XXRycN+h44GPs9kuSqa2qGazIHs4ls5z2'.
+           'hen5GqclVORiYBVVzVRl/ygqMxxPG1xY13mcKpHUDar8ORX9VNP2F+Hf9BM=';
+
+$css = <<< EOF
+body {
+  margin: 0;
+  padding: 0;
+}
+main {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  flex-grow: 1;
+}
+#text {
+  height: 60vh;
+  z-index: 1;
+}
+h1, h2 {
+  font-family: "Myriad Pro", "Helvetica Neue", "Helvetica", "Frutiger Neue", 
+               "Frutiger", sans-serif;
+  font-weight: 300;
+  text-align: center;
+  display: block;
+}
+h1 {
+  font-size: 2rem;
+  margin-top: 30vh;
+  line-height: 2.3rem;
+}
+h2 {
+  font-size: 1.25rem;
+  line-height: 1.7rem;
+}
+.image-box {
+  margin: 0 auto;
+}
+.image-box > img {
+  display: block;
+  height: 220px;
+  width: 220px;
+  order: 1;
+  z-index: 5;
+}
+EOF;
+
+function inline_pic($img, $type)
+{
+  $img = gzinflate(base64_decode($img));
+  return sprintf("\"data:image/%s;base64,%s\"", $type, base64_encode($img));
+}
+
+
+
+?>
+<!doctype html>
+<html>
+<head>
+  <title>welcome to <?=$host ?></title>
+  <link href=<?=inline_pic($favicon,"x-icon")?> rel="icon" type="image/x-icon"/>
+  <style>
+    <?=$css ?>
+  </style>
+</head>
+<body>
+  <main>
+    <div id="text">
+      <h1>
+        Welcome to <?=$host?>.
+      </h1>
+      <h2>
+        This site has been set up successfully.
+        The document root is <?=__DIR__?>/.
+      </h2>
+    </div>
+    <div class="image-box">
+      <img src=<?=inline_pic($ducky, "jpeg")?>>
+    </div>
+  </main>
+</body>
+</html>
